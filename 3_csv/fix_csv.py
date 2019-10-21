dcc7345767d480ab81d8db4ebc11ded8943fe704
Ryan Horton <rhorton@scitec.com>
2019-10-21 13:12:49 -0600

    Did a version of week 3 that I am happy with. The instructions/tests are a bit confusing so we'll see what the soln looks like. But the argparse library is super cool.

---
+++ 3_csv/fix_csv.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+# required arguments
+parser.add_argument("source", help="source file to be converted.")
+parser.add_argument("destination", help="destination file to be created.")
+# optional arguments
+parser.add_argument("--in-delimiter", help="delimiter in source file.", default="|")
+parser.add_argument("--out-delimiter", help="delimiter in destination file.", default=",")
+
+args = parser.parse_args()
+
+with open(args.source, 'r') as read_from:
+    with open(args.destination, 'w') as write_to:
+        for line in read_from:
+            write_to.write(line.replace(args.in_delimiter,args.out_delimiter))
