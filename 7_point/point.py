5c900f2d40aa3d9fa782d8079015d7af965bb4d1
Ryan Horton <rhorton@scitec.com>
2019-11-22 12:43:48 -0700

    Amendments to point after reading solutions yesterday.

--- 7_point/point.py
+++ 7_point/point.py
@@ -3,9 +3,7 @@ class Point:
 
     def __init__(self, x, y, z):
         """Initialize a point object from x,y, and z coordinates"""
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x, self.y, self.z = (x, y, z)
 
     def __repr__(self):
         """Define string representation of Points."""
@@ -13,32 +11,19 @@ class Point:
 
     def __eq__(self, other):
         """Two Points are equal iff their x,y and z coordinates are the same."""
-        result = True
-        if not isinstance(other, Point):
-            result = False
-        elif self.x != other.x:
-            result = False
-        elif self.y != other.y:
-            result = False
-        elif self.z != other.z:
-            result = False
-        return result
+        return tuple(self) == tuple(other)
     
     def __sub__(self, other):
         """Subtract x,y, and z coordiante of two Points (new_x = x1-x2 etc)."""
-        if not isinstance(other, Point):
-            raise ValueError("Tried to subtract a Point and a non-Point")
-        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+        return Point(*(c1-c2 for (c1, c2) in zip(self, other)))
 
     def __add__(self, other):
         """Add x,y, and z coordiante of two Points (new_x = x1+x2 etc)."""
-        if not isinstance(other, Point):
-            raise ValueError("Tried to add a Point and a non-Point")
-        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+        return Point(*(c1+c2 for (c1, c2) in zip(self, other)))
 
     def __mul__(self, other):
         """Multiply each coordiante of a Point by a scalar"""
-        return Point(self.x * other, self.y * other, self.z * other)
+        return Point(*(coord*other for coord in self))
 
     def __rmul__(self, other):
         """Multiplication if Point is on the right side of the operation."""
@@ -46,4 +31,4 @@ class Point:
 
     def __iter__(self):
         """Allow unpacking of points (i.e. p1 = Point(1,2,3); x, y, z = p1)"""
-        return iter((self.x, self.y, self.z))
+        yield from (self.x, self.y, self.z)
