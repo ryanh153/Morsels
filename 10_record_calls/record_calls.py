57794ef617c28697cd9835657848d67f52654e65
Ryan Horton <rhorton@scitec.com>
2019-12-12 12:37:49 -0700

    Updated. Dataclass is cool and I should try and learn it better. dataclass/named tuple makes making minimilist classes for storing data easy.

--- 10_record_calls/record_calls.py
+++ 10_record_calls/record_calls.py
@@ -1,35 +1,33 @@
 import functools
-from collections import namedtuple
+from dataclasses import dataclass
+from typing import Any, Optional
 
 NO_RETURN = object()
+
+@dataclass
+class Call:
+    args: tuple
+    kwargs: dict
+    return_value: Any = NO_RETURN  # type is any, default is NO_RETURN
+    exception: Optional[BaseException] = None  # type is optionally BaseException, but its default it none
 
 
 def record_calls(func):
     """Decorator to track number of times a function is called"""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        wrapper.calls.append(namedtuple("Call", "args, kwargs, return_value, exception"))  # new named tuple
         wrapper.call_count += 1  # these three are the same whether or not exception is thrown
-        wrapper.calls[-1].args = args
-        wrapper.calls[-1].kwargs = kwargs
+        call = Call(args, kwargs)
+        wrapper.calls.append(call)
         try:  # return_value and exception depend on success of call
-            value = func(*args, **kwargs)
-            wrapper.calls[-1].return_value = value  # capture value
-            wrapper.calls[-1].exception = None  # no exception
+            call.return_value = func(*args, **kwargs)
         except Exception as ex:
-            wrapper.calls[-1].return_value = NO_RETURN  # unique value to show what happened
-            wrapper.calls[-1].exception = ex  # capture exception
+            call.exception = ex
             raise ex  # raise exception
-        return value
+        return call.return_value
 
     # Set up defaults
     wrapper.call_count = 0
     wrapper.calls = []  # will be a list of named tuples
 
     return wrapper
-
-
-@record_calls
-def greet(name):
-    """Test func"""
-    print(f"Hello {name}")
