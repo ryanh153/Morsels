613bd4f279e99fbe6df86c0e0e0edf8274aee22d
Ryan Horton <rhorton@scitec.com>
2020-02-13 16:11:20 -0700

    Updates day after reading window solution.

--- 17_window/window.py
+++ 17_window/window.py
@@ -1,20 +1,18 @@
-from collections.abc import Sequence
+from collections import deque
 
 
 def window(iterable, n, *, fillvalue=None):
-    held_list, start = [], -1
-    if n == 0:
-        yield from []
-    elif isinstance(iterable, Sequence):
-        if n > len(iterable):
-            iterable += [fillvalue for _ in range(n-len(iterable))]
-        yield from [tuple(iterable[i:i+n]) for i in range(len(iterable)-(n-1))]
-    else:
-        while True:
-            start += 1
-            stop = start + n
-            try:
-                held_list += [next(iterable) for _ in range(stop - len(held_list))]
-                yield tuple(held_list[start:stop])
-            except StopIteration:
-                break
+    if n == 0:  # return nothing if n==0
+        return
+
+    iterator = iter(iterable)  # set up iterable and deque
+    current = deque(maxlen=n)
+
+    for _ in range(n):  # get first return
+        current.append(next(iterator, fillvalue))
+    yield tuple(current)
+
+    for val in iterator:  # yield further returns
+        current.append(val)
+        if len(current) == n:
+            yield tuple(current)
