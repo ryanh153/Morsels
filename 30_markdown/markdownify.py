b1f499fc7b32b64c645036b27ee4b780a38c9e4f
Ryan Horton <rhorton@scitec.com>
2020-05-14 11:17:34 -0600

    Updates to markdownify (#30).

--- 30_markdown/markdownify.py
+++ 30_markdown/markdownify.py
@@ -1,13 +1,16 @@
 import re
 
+WHITESPACE_RE = re.compile(r"\s+")
+LINEBREAK_RE = re.compile(r"<br>")
+STRONG_RE = re.compile(r"</?strong>")
+LINK_RE = re.compile(r'<a href="(.*?)">(.*?)</a>')
+PARAGRAPH_RE = re.compile(r"</?p>")
+
 
 def markdownify(text):
-    text = re.sub(r"[ \n]+", " ", text)  # replace one or more spaces/new lines with one whitespace character
-    text = re.sub(r"<br>", "  \n", text)  # <br> tag means two spaces followed by a new line
-    text = re.sub(r"</*strong>", "**", text)  # opening or closing <strong> tag with "**"
-    link_re = re.compile(r'<a href="([^"]+)">([^<]+)</a>')
-    matches = link_re.findall(text)  # get the hyperlink and name strings
-    for match in matches:  # replace each one sequentially
-        text = link_re.sub(f"[{match[1]}]({match[0]})", text, count=1)
-    text = re.sub(r"<p>", "", re.sub(r"</p>", "\n\n", text)).rstrip()  # two new lines after paragraph (except last)
-    return text
+    text = WHITESPACE_RE.sub(" ", text)  # replace one or more spaces/new lines with one whitespace character
+    text = PARAGRAPH_RE.sub("\n", text)  # replace start/end paragraph tags with one new line each
+    text = LINEBREAK_RE.sub("  \n", text)  # <br> tag means two spaces followed by a new line
+    text = STRONG_RE.sub("**", text)  # opening or closing <strong> tag with "**"
+    text = LINK_RE.sub(r"[\2](\1)", text)  # get the hyperlink and name strings
+    return text.strip()
