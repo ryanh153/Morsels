7dd09a71d53edc70cb353df6311292c098fa92fd
Ryan Horton <rhorton@scitec.com>
2020-05-29 12:53:18 -0600

    Fixed float_range2 to better fit the idea of what was supposed to be tested. I really like it.

--- 31_float_range_2/float_range2.py
+++ 31_float_range_2/float_range2.py
@@ -1,40 +1,30 @@
 class float_range:
 
-    def __init__(self, *args):
-        if len(args) == 3:  # passed all arguments
-            self.start, self.stop, self.step = args
-        elif len(args) == 2:  # passed start/stop
-            self.start, self.stop, self.step = *args, 1
-        elif len(args) == 1:  # passed stop
-            self.start, self.stop, self.step = 0, *args, 1
-        else:
-            raise TypeError("float_range expected 1, 2 or 3 arguments. Got 0 or more than 3.")
+    def __init__(self, start, stop=None, step=1):
+        if stop is None:
+            start, stop = 0, start
+        self.start, self.stop, self.step = start, stop, step
+        self._calculate_length()
 
-        self.steps = 0
-        self._list = [self.start + self.step*i for i in range(len(self))]
+    def _calculate_length(self):
+        self._len = int((self.stop - self.start) / self.step)
+        if self.start + self.step * self._len != self.stop:
+            self._len += 1  # does not hit end, so we do return the last element before stop is passed
+        self._len = max([0, self._len])
 
     def __len__(self):
-        steps = int((self.stop-self.start)/self.step)
-        if self.start + self.step*steps != self.stop:
-            steps += 1  # does not hit end, so we do return the last element before stop is passed
-        return max([0, steps])
+        return self._len
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.steps == len(self):
-            self.steps = 0
-            raise StopIteration
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            start, stop, step = index.indices(len(self))
+            new_start = self.start + start*self.step
+            new_stop = self.start + stop*self.step
+            new_step = step*self.step
+            return float_range(new_start, new_stop, new_step)
+        if 0 <= index < len(self):
+            return self.start + self.step*index
+        elif 0 > index >= -1*len(self):
+            return self.start + self.step*(len(self)+index)
         else:
-            self.steps += 1
-            return self.start + self.step*(self.steps-1)
-
-    def __getitem__(self, my_slice):
-        return self._list[my_slice]
-
-    def __reversed__(self):
-        start = self._list[-1]
-        step = -1 * self.step
-        stop = self._list[0] + 0.5*step  # need to include the last item in the stored list
-        return float_range(start, stop+0.5*step, step)
+            raise IndexError("Index is out of range")
