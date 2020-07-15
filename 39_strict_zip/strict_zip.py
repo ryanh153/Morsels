948ebb51476813da64bbc2c0b1f91f80a8f20338
Ryan Horton <rhorton@scitec.com>
2020-07-15 11:31:05 -0600

    Updated zip_strict after reading solutions yesterday.

--- 39_strict_zip/strict_zip.py
+++ 39_strict_zip/strict_zip.py
@@ -1,18 +1,31 @@
+# My first past with no outside libraries. I like it
+# def strict_zip(*sequences):
+#     """Zip an arbitrary number of iterables that are the same length together."""
+#     iterators = [iter(s) for s in sequences]
+#     while next_tuple := get_next(iterators):
+#         yield next_tuple
+#
+#
+# def get_next(iterators):
+#     """Make a tuple that is the next element of each iterator. If only some have a next value raise a value error"""
+#     result = []
+#     for i in iterators:
+#         try:
+#             result.append(next(i))
+#         except StopIteration:
+#             pass
+#     if len(result) == len(iterators) or not len(result):
+#         return tuple(result)
+#     raise ValueError('Value out of range')
+
+# A clean solution using other zip tools
+from itertools import zip_longest
+
+
 def strict_zip(*sequences):
-    """Zip an arbitrary number of iterables that are the same length together."""
-    iterators = [iter(s) for s in sequences]
-    while next_tuple := get_next(iterators):
-        yield next_tuple
+    empty = object()
+    for result in zip_longest(*sequences, fillvalue=empty):
+        if empty in result:
+            raise ValueError("Sequences are not all the same length")
+        yield result
 
-
-def get_next(iterators):
-    """Make a tuple that is the next element of each iterator. If only some have a next value raise a value error"""
-    result = []
-    for i in iterators:
-        try:
-            result.append(next(i))
-        except StopIteration:
-            pass
-    if len(result) == len(iterators) or not len(result):
-        return tuple(result)
-    raise ValueError('Value out of range')
