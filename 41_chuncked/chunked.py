168220aab25359eeb18e5a32d8486c1215db8997
Ryan Horton <rhorton@scitec.com>
2020-07-28 09:54:33 -0600

    Redid chunked with 3rd party libraries

--- 41_chuncked/chunked.py
+++ 41_chuncked/chunked.py
@@ -1,15 +1,29 @@
-sentinal = object()
+from itertools import zip_longest
 
 
-def chunked(iterable, n, *, fill=sentinal):
-    next_list = []
-    for val in iterable:
-        next_list.append(val)
-        if len(next_list) >= n:
-            yield next_list
-            next_list = []
-    if next_list:
-        if fill is not sentinal:
-            for _ in range(n-len(next_list)):
-                next_list.append(fill)
-        yield next_list
+SENTINAL = object()
+
+# No libraries
+#def chunked(iterable, n, *, fill=sentinal):
+#     next_list = []
+#     for val in iterable:
+#         next_list.append(val)
+#         if len(next_list) >= n:
+#             yield next_list
+#             next_list = []
+#     if next_list:
+#         if fill is not sentinal:
+#             for _ in range(n-len(next_list)):
+#                 next_list.append(fill)
+#         yield next_list
+
+
+# Some libraries and slight compact
+def chunked(iterable, n, *, fill=SENTINAL):
+    for next_tuple in zip_longest(*([iter(iterable)] * n), fillvalue=SENTINAL):
+        if SENTINAL not in next_tuple:
+            yield next_tuple
+        elif fill is SENTINAL:
+            yield tuple(v for v in next_tuple if v is not SENTINAL)
+        else:
+            yield tuple(v if v is not SENTINAL else fill for v in next_tuple)
