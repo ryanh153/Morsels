3e11086302d1b9536536bcb61ecc8254bd6045ce
Ryan Horton <rhorton@scitec.com>
2020-07-30 11:51:05 -0600

    Cleaner solution after reading.
    Rembmer returning generators and using yields are both good ways
    to make an iterator and can but used in different situations to
    achieve more succinctness and/or compactness. One is not always
    better.

--- 41_chuncked/chunked.py
+++ 41_chuncked/chunked.py
@@ -3,27 +3,9 @@ from itertools import zip_longest
 
 SENTINAL = object()
 
-# No libraries
-#def chunked(iterable, n, *, fill=sentinal):
-#     next_list = []
-#     for val in iterable:
-#         next_list.append(val)
-#         if len(next_list) >= n:
-#             yield next_list
-#             next_list = []
-#     if next_list:
-#         if fill is not sentinal:
-#             for _ in range(n-len(next_list)):
-#                 next_list.append(fill)
-#         yield next_list
-
 
 # Some libraries and slight compact
 def chunked(iterable, n, *, fill=SENTINAL):
-    for next_tuple in zip_longest(*([iter(iterable)] * n), fillvalue=SENTINAL):
-        if SENTINAL not in next_tuple:
-            yield next_tuple
-        elif fill is SENTINAL:
-            yield tuple(v for v in next_tuple if v is not SENTINAL)
-        else:
-            yield tuple(v if v is not SENTINAL else fill for v in next_tuple)
+    iterables = [iter(iterable)] * n
+    chunks = zip_longest(*iterables, fillvalue=fill)
+    return ([val for val in chunk if val is not SENTINAL] for chunk in chunks)
