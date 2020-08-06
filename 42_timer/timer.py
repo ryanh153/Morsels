43c1e41d1e21bd51ff1ec908674c12e9b3973f69
Ryan Horton <rhorton@scitec.com>
2020-08-06 10:25:12 -0600

    Timer done and made small effeciency updates after reading solutions

--- 42_timer/timer.py
+++ 42_timer/timer.py
@@ -1,29 +1,36 @@
-from datetime import datetime
+from time import perf_counter
 from statistics import mean, median
 
 
 class Timer:
     def __init__(self, function=None):
         self.function = function  # Function to decorate if being use as a decorator
-        self.start, self.elapsed = None, None
-        self.runs = list()
-        self.min, self.max, self.mean, self.median = None, None, None, None
+        self.start, self.elapsed, self.runs = None, None, list()
+
+    @property
+    def min(self):
+        return min(self.runs)
+
+    @property
+    def max(self):
+        return max(self.runs)
+
+    @property
+    def mean(self):
+        return mean(self.runs)
+
+    @property
+    def median(self):
+        return median(self.runs)
 
     def __enter__(self):
-        self.start = datetime.now()
+        self.start = perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.elapsed = (datetime.now() - self.start).total_seconds()
+        self.elapsed = perf_counter() - self.start
         self.runs.append(self.elapsed)
-        self._update_stats()
-
-    def _update_stats(self):
-        self.min = min(self.runs)
-        self.max = max(self.runs)
-        self.mean = mean(self.runs)
-        self.median = median(self.runs)
 
     def __call__(self, *args, **kwargs):
-        with self as timer:
+        with self:
             return self.function(*args, **kwargs)
