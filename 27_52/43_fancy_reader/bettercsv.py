8fee24d175dcdf631107d7daa37a52dce759241e
Ryan Horton <rhorton@scitec.com>
2020-08-14 12:24:17 -0600

    Cleanup for bettercsv. This one was really interesting to trim down

--- 27_52/43_fancy_reader/bettercsv.py
+++ 27_52/43_fancy_reader/bettercsv.py
@@ -1,44 +1,22 @@
 from csv import reader
+from collections import namedtuple
 
 
 class FancyReader:
     def __init__(self, str_iter, fieldnames=None):
-        self.str_iter = iter(str_iter)
-        if fieldnames is None:
-            self.fieldnames = [name for name in list(reader([next(self.str_iter)], delimiter=',', quotechar='"'))[0]]
-            self.line_num = 1
-        else:
-            self.fieldnames = fieldnames
-            self.line_num = 0
+        self.reader = reader(str_iter, delimiter=',', quotechar='"')
+        self.line_num = 0
+        self.fieldnames = fieldnames
+        self.Row = None
 
     def __next__(self):
+        if self.Row is None:
+            if self.fieldnames is None:
+                self.line_num += 1
+                self.fieldnames = next(self.reader)
+            self.Row = namedtuple('Row', self.fieldnames)
         self.line_num += 1
-        return FancyLine(next(self.str_iter), self.fieldnames)
+        return self.Row(*next(self.reader))
 
     def __iter__(self):
         return self
-
-
-class FancyLine:
-    def __init__(self, csv_str, fieldnames=None):
-        line_data = list(reader([csv_str], delimiter=',', quotechar='"'))[0]
-        self.data = list(zip(line_data, fieldnames))
-        self.index = 0
-
-    def __getattr__(self, item):
-        for value, name in self.data:
-            if name == item:
-                return value
-
-    def __next__(self):
-        if self.index < len(self.data):
-            self.index += 1
-            return self.data[self.index - 1][0]
-        else:
-            raise StopIteration
-
-    def __iter__(self):
-        return self
-
-    def __repr__(self):
-        return f"Row({', '.join('='.join([name, repr(val)]) for val, name in self.data)})"
