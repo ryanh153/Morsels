b9d86801530246af5bf05c5665aaf4ac7b253579
Ryan Horton <rhorton@scitec.com>
2020-09-03 09:50:17 -0600

    Minor update to enforce passed arguments are files that can be read as bytes

--- 27_52/46_implicit_concat/concat_check.py
+++ 27_52/46_implicit_concat/concat_check.py
@@ -1,17 +1,17 @@
-import argparse
+from argparse import ArgumentParser, FileType
 from tokenize import tokenize
 from token import STRING, NL
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('files', nargs='+', help='Files to have concat check run on')
+    parser = ArgumentParser()
+    parser.add_argument('files', nargs='+', type=FileType('rb'), help='Files to have concat check run on')
     args = parser.parse_args()
 
     for file in args.files:
         prev_string, prev_type, prev_row = None, None, None
-        for tk_data in tokenize(open(file, 'rb').readline):
+        for tk_data in tokenize(file.readline):
             if prev_type == tk_data.type == STRING:
-                print(f'{file}, line {prev_row} between {prev_string} and {tk_data.string}')
+                print(f'{file.name}, line {prev_row} between {prev_string} and {tk_data.string}')
             if tk_data.type != NL:  # don't update on new line to catch multi-line implicit concatenation
                 prev_string, prev_type, prev_row = tk_data.string, tk_data.type,  tk_data.end[0]
