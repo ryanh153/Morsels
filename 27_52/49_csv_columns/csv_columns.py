2076baa331f3ec5a85a8b9ae155df33da27671e5
Ryan Horton <rhorton@scitec.com>
2020-09-24 12:01:13 -0600

    Went over csv reader and kept two solutions that are both elegant

--- 27_52/49_csv_columns/csv_columns.py
+++ 27_52/49_csv_columns/csv_columns.py
@@ -1,13 +1,22 @@
-from itertools import zip_longest
 import csv
+from itertools import zip_longest
+from collections import defaultdict
 
 
 def csv_columns(file_obj, headers=None, missing=None):
     reader = csv.reader(file_obj, delimiter=',', quotechar='"')
     if headers is None:
         headers = next(reader)
-    result = {h: [] for h in headers}
-    for line in reader:
-        for key, val in zip_longest(headers, line, fillvalue=missing):
-            result[key].append(val)
+    return {
+        header: list(data)
+        for header, *data in zip_longest(headers, *reader, fillvalue=missing)
+    }
+
+
+def csv_columns_dict_reader(file_obj, headers=None, missing=None):
+    reader = csv.DictReader(file_obj, fieldnames=headers, restval=missing, delimiter=',', quotechar='"')
+    result = defaultdict(list)
+    for row in reader:
+        for header, value in row.items():
+            result[header].append(value)
     return result
