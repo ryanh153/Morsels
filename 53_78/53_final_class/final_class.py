e7e3d9b84b2cff7fdbc25b1ead9b68c85abc7d29
ryan <ryan.h153@gmail.com>
2020-10-29 14:52:33 -0600

    Redid mostly following solution. Really interesting week

--- 53_78/53_final_class/final_class.py
+++ 53_78/53_final_class/final_class.py
@@ -1,22 +1,18 @@
 class UnsubclassableType(type):
-    def __new__(mcs, name, base, dct):
-        for b in base:
-            if type(b) == mcs:
-                raise TypeError(f'Class {b.__name__} cannot be inherited from')
-        new_cls = super().__new__(mcs, name, base, dct)
-        return new_cls
+    def __new__(cls, name, bases, namespace):
+        if any(type(b) == UnsubclassableType for b in bases):
+            raise TypeError(f'Class {cls.__name__} is not a valid base class')
+        return super().__new__(cls, name, bases, namespace)
 
 
-class Unsubclassable(metaclass=UnsubclassableType):
+def final_class(cls):
+    @classmethod
+    def no_subclass(subclass):
+        raise TypeError(f'Class {subclass.__name__} is trying to inherit from Class {cls.__name__} which is not allowed')
+    cls.__init_subclass__ = no_subclass
+    return cls
+
+
+@final_class
+class Unsubclassable():
     pass
-
-
-class final_class():
-    def __init__(self, name, base=(), dct=None):
-        for b in base:
-            if type(b) == type(self):
-                raise TypeError('Can not sub from class decorated by final_class')
-        self.decorated_class = name
-
-    def __call__(self, *args, **kwargs):
-        return self.decorated_class(*args, **kwargs)
