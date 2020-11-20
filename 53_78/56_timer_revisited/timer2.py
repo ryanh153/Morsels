a21a96e4e5ec3c6db5cadbd5e2c97c3779365484
Ryan Horton <rhorton@scitec.com>
2020-11-20 07:59:45 -0700

    Updated timer the day after reading solutions.

--- 53_78/56_timer_revisited/timer2.py
+++ 53_78/56_timer_revisited/timer2.py
@@ -1,56 +1,43 @@
 from time import perf_counter
 
 
-_memoized = {}
-
-
-def memoize(cls):
-    def memoized(name=None):
-        print(f'have {_memoized}\nmaking {name}')
-        if name and name not in _memoized:
-            _memoized[name] = cls()
-            return _memoized[name]
-        elif name:
-            return _memoized[name]
-        else:
-            return cls()
-    return memoized
-
-
-@memoize
 class Timer:
+    named_timers = {}
 
-    def __init__(self):
-        self.start, self.stop, self.elapsed = None, None, None
-        self.splits, self.runs = [], []
-        self.stored_runs = {}
+    def __new__(cls, name=None):
+        if name is None:
+            return super().__new__(cls)
+        elif name not in cls.named_timers:
+            cls.named_timers[name] = super().__new__(cls)
+        return cls.named_timers[name]
+
+    def __init__(self, name=None):
+        if not hasattr(self, 'running'):
+            self.running = False
+            self.start, self.stop, self.elapsed = None, None, None
+            self.runs, self.splits, self.named_splits = [], [], {}
 
     def __enter__(self):
+        self.running = True
         self.start = perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop = perf_counter()
+        self.running = False
         self.elapsed = self.stop - self.start
         self.runs.append(self.elapsed)
-        self.start, self.stop = None, None
 
     def split(self, name=None):
-        if self.start is None:
+        if not self.running:
             raise RuntimeError('Timer is not running!')
         if name:
-            if name not in self.stored_runs:
-                new_timer = Timer()
-                self.stored_runs[name] = new_timer
-            return self.stored_runs[name]
+            return self.named_splits.setdefault(name, Timer())
         else:
-            new_timer = Timer()
-            self.splits.append(new_timer)
-            return new_timer
+            self.splits.append(Timer())
+            return self.splits[-1]
 
     def __getitem__(self, key):
         if isinstance(key, str):
-            return self.stored_runs[key]
-        elif isinstance(key, int):
-            return self.splits[key]
-        return NotImplemented
+            return self.named_splits[key]
+        return self.splits[key]
