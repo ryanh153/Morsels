8a2b7a326c37e1d3d546708e942ae2f13f6c5086
Ryan Horton <rhorton@scitec.com>
2020-12-28 09:57:11 -0700

    Updated after reading solution

--- 53_78/59_proxy_dict/proxydict.py
+++ 53_78/59_proxy_dict/proxydict.py
@@ -1,59 +1,25 @@
-class ProxyDict:
+from collections.abc import Mapping
+from collections import ChainMap
 
-    MISSING = object()
+
+class ProxyDict(Mapping):
 
     def __init__(self, *input_dicts):
-        self.data = [input_dict for input_dict in input_dicts[::-1]]
-
-    def keys(self):
-        yield from [key for sub_dict in self.data for key in sub_dict.keys()]
-
-    def items(self):
-        yield from [item for sub_dict in self.data for item in sub_dict.items()]
-
-    def values(self):
-        yield from [value for sub_dict in self.data for value in sub_dict.values()]
-
-    def get(self, key, default=None):
-        for sub_dict in self.data:
-            if key in sub_dict:
-                return sub_dict[key]
-        else:
-            return default
+        self.data = ChainMap(*input_dicts[::-1])
 
     def __getitem__(self, key):
-        result = self.get(key, self.MISSING)
-        if result is not self.MISSING:
-            return result
-        else:
-            raise KeyError(f'Key: {key} is not in {type(self).__name__}')
-
-    def __setitem__(self, key, value):
-        raise TypeError(f'{type(self).__name__} does not support item assignment')
-
-    def __eq__(self, other):
-        if isinstance(other, dict):
-            for key in other:
-                if key not in self or self[key] != other[key]:
-                    return False
-            else:
-                return True
-        elif isinstance(other, ProxyDict):
-            return self.data == other.data
-        else:
-            return NotImplemented
+        return self.data[key]
 
     def __len__(self):
-        return sum([len(sub_dict) for sub_dict in self.data])
+        return len(self.data)
 
     def __iter__(self):
-        for sub_dict in self.data:
-            yield from sub_dict.keys()
+        yield from self.data.keys()
 
     def __repr__(self):
-        members = ', '.join(repr(sub_dict) for sub_dict in self.data[::-1])
+        members = ', '.join(repr(my_map) for my_map in self.data.maps[::-1])
         return f"{type(self).__name__}({members})"
 
     @property
     def maps(self):
-        return [sub_dict for sub_dict in self.data[::-1]]
+        return self.data.maps[::-1]
