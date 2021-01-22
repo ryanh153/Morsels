38bf42a0791d974049d82bd9fd66798357503a49
Ryan Horton <rhorton@scitec.com>
2021-01-22 10:00:18 -0700

    Cleaned up month (a lot) after reading solutions

--- 53_78/61_month/month.py
+++ 53_78/61_month/month.py
@@ -1,62 +1,25 @@
-from functools import total_ordering
 import datetime
 from calendar import monthrange
-import inspect
+from dataclasses import dataclass
 
 
-@total_ordering
+@dataclass(frozen=True, order=True)
 class Month:
-    __slots__ = ['year', 'month', 'month_str', 'first_day', 'last_day']
+    __slots__ = ['year', 'month', 'first_day', 'last_day']
+    year: int
+    month: int
 
-    def __init__(self, year, month):
-        self.year = year
-        self.month = month
-        if self.month < 10:
-            self.month_str = f'0{month}'
-        else:
-            self.month_str = str(month)
+    def __post_init__(self):
+        _, last = monthrange(self.year, self.month)
+        super().__setattr__('first_day', datetime.date(self.year, self.month, 1))
+        super().__setattr__('last_day', datetime.date(self.year, self.month, last))
 
-        _, last = monthrange(year, month)
-        self.first_day = datetime.date(year, month, 1)
-        self.last_day = datetime.date(year, month, last)
+    @classmethod
+    def from_date(cls, date):
+        return cls(date.year, date.month)
 
-    @staticmethod
-    def from_date(date):
-        instance = Month(date.year, date.month)
-        return instance
-
-    def strftime(self, format):
-        return self.first_day.strftime(format)
+    def strftime(self, fmt):
+        return self.first_day.strftime(fmt)
 
     def __str__(self):
-        return f'{self.year}-{self.month_str}'
-
-    def __repr__(self):
-        return f'{type(self).__name__}({self.year}, {self.month})'
-
-    def __eq__(self, other):
-        if not isinstance(other, Month):
-            return NotImplemented
-        return self.year == other.year and self.month == other.month
-
-    def __lt__(self, other):
-        if not isinstance(other, Month):
-            return NotImplemented
-        if self.year < other.year:
-            return True
-        elif self.year == other.year and self.month < other.month:
-            return True
-        else:
-            return False
-
-    def __setattr__(self, *args):
-        if inspect.stack()[1][3] == '__init__':
-            object.__setattr__(self, *args)
-        else:
-            raise TypeError(f'{type(self).__name__} is immutable')
-
-    def __delattr__(self, item):
-        raise TypeError(f'{type(self).__name__} is immutable')
-
-    def __hash__(self):
-        return hash(repr(self))
+        return f'{self.year}-{self.month:02}'
