dbaadbcaeb6c6118b50934ae2d4b06607cfb8f21
Ryan Horton <rhorton@scitec.com>
2021-02-12 13:41:37 -0700

    Updated sorted list

--- 53_78/63_sorted_list/sortedlist.py
+++ 53_78/63_sorted_list/sortedlist.py
@@ -1,76 +1,51 @@
-from bisect import bisect_left, bisect
+from bisect import bisect_left, bisect, insort
+import sys
 
 
 class SortedList:
 
     def __init__(self, data, key=lambda x: x):
-        self.data = list(data)
-        self.key_func = key
-        self.data.sort(key=self.key_func)
-        self.keys = [self.key_func(item) for item in self.data]
+        self.key = key
+        self.data = sorted(((key(d), d) for d in data))
+
+    def _entry(self, item):
+        return self.key(item), item
 
     def add(self, item):
-        self.data.insert(bisect_left(self.keys, self.key_func(item)), item)
-        self.keys.insert(bisect_left(self.keys, self.key_func(item)), self.key_func(item))
+        insort(self.data, self._entry(item))
 
     def remove(self, item):
-        index = bisect_left(self.keys, self.key_func(item))
-        if index != self.__len__() and self.data[index] == item:
-            self.data.pop(index)
-            self.keys.pop(index)
-        else:
-            raise ValueError
+        del self.data[self.index(item)]
+
+    def index(self, item, start=0, stop=sys.maxsize):
+        stop = min(stop, self.__len__())
+        index = bisect_left(self.data, self._entry(item), lo=start, hi=stop)
+        if index != stop and self.data[index] == self._entry(item):
+            return index
+        raise ValueError
 
     def __contains__(self, item):
-        index = bisect_left(self.keys, self.key_func(item))
-        return index != self.__len__() and self.data[index] == item
-
-    def index(self, item, start=0, stop=None):
-        if stop is None or stop > self.__len__():
-            stop = self.__len__()
-        index = bisect_left(self.keys, self.key_func(item), lo=start, hi=stop)
-
-        if index != stop and self.data[index] == item:
-            return index
-        else:
-            raise ValueError
+        index = bisect_left(self.data, self._entry(item))
+        return index != self.__len__() and self.data[index] == self._entry(item)
 
     def __repr__(self):
-        return f'SortedList({repr(self.data)})'
-
-    def __iter__(self):
-        return iter(self.data)
-
-    def __next__(self):
-        yield from self.data
+        return f'{type(self).__name__}({repr([d for k, d in self.data])})'
 
     def __getitem__(self, index):
-        return self.data.__getitem__(index)
+        return self.data.__getitem__(index)[1]
 
     def __len__(self):
         return len(self.data)
 
     def find(self, item):
-        index = bisect_left(self.keys, self.key_func(item))
-        if index != self.__len__() and self.data[index] == item:
-            return index
-        else:
-            return -1
+        index = bisect_left(self.data, self._entry(item))
+        return index if index != len(self) and self.data[index] == self._entry(item) else -1
 
     def rfind(self, item):
-        index = bisect(self.keys, self.key_func(item))
-        if self.data[index-1] == item:
-            return index - 1
-        else:
-            return -1
+        index = bisect(self.data, self._entry(item))
+        return index-1 if self.data[index-1] == self._entry(item) else -1
 
     def count(self, item):
-        index = bisect_left(self.keys, self.key_func(item))
-        count = 0
-        for val in self.data[index:]:
-            if self.key_func(val) == self.key_func(item):
-                if val == item:
-                    count += 1
-            else:
-                break
-        return count
+        index_left = bisect_left(self.data, self._entry(item))
+        index_right = bisect(self.data, self._entry(item))
+        return index_right - index_left
