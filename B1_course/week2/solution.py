1f641bc9afd9dc1188bef517838726cfb527550a
Ryan Horton <rhorton@scitec.com>
2021-03-11 09:36:08 -0700

    Updates to 2 and 3 that maximize function re-use and unpacking

--- B1_course/week2/solution.py
+++ B1_course/week2/solution.py
@@ -1,23 +1,10 @@
-def interp_args(start, stop, step):
-    if stop is None:
+def myrange3(start, stop, step):
+    if not step:
         start, stop = 0, start
-    return start, stop, step
-
-
-def myrange(start, stop, step):
-    val = start
-    while val < stop:
-        yield val
-        val += step
+    while start < stop:
+        yield start
+        start += step
 
 
 def myrange2(start, stop=None, step=1):
-    start, stop, step = interp_args(start, stop, step)
-    # return list(range(start, stop, step))
-    return list(myrange(start, stop, step))
-
-
-def myrange3(start, stop=None, step=1):
-    start, stop, step = interp_args(start, stop, step)
-    # yield from range(start, stop, step)
-    yield from myrange(start, stop, step)
+    return list(myrange3(start, stop, step))
--- B1_course/week3/solution.py
+++ B1_course/week3/solution.py
@@ -4,21 +4,15 @@ import re
 def re_logtolist(data):
     result = []
     for line in data:
-        values = line_parse(line)
-        if values is not None:
-            ip_address, timestamp, request = values
-        else:
-            ip_address, timestamp, request = 'No IP address found', 'No timestamp found', 'No request found'
-
+        ip_address, timestamp, request = line_parse(line)
         result.append({'ip_address': ip_address, 'timestamp': timestamp, 'request': request})
-
     return result
 
 
 def line_parse(line):
     match = re.search(re_capture, line)
     if match is None:
-        return match
+        return 'No IP address found', 'No timestamp found', 'No request found'
     else:
         return match.group(1), match.group(2), match.group(3)
 
