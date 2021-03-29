fc329ea9536f4cef3fe0a1fddcc4d779bd3342fa
Ryan Horton <rhorton@scitec.com>
2021-03-29 16:38:09 -0600

    Updated B1 soln 5 to use extrasaction instead of filtering each dict ourselves

--- B1_course/week5/solution.py
+++ B1_course/week5/solution.py
@@ -1,15 +1,12 @@
 import requests
-import json
 import csv
 
 
-gist_url = 'https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw' \
-           '/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json '
+gist_url = 'https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 KEYS = ['city', 'state', 'rank', 'population']
 
 
 def cities_to_csv(url, filename):
-    data = json.loads(requests.get(url).text)
     with open(filename, 'w+') as of:
-        dw = csv.DictWriter(of, fieldnames=KEYS, delimiter='\t')
-        dw.writerows([{k: row[k] for k in KEYS} for row in data])
+        dw = csv.DictWriter(of, fieldnames=KEYS, delimiter='\t', extrasaction='ignore')
+        dw.writerows(requests.get(url).json())
