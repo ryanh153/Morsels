2a9df7c71148a24f5a0d3acfe7338e7a870b6921
Ryan Horton <rhorton@scitec.com>
2021-04-07 12:43:31 -0600

    Minor cleanups

--- B1_course/week7/solution.py
+++ B1_course/week7/solution.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def file_func(dir_path, func):
     dir_path = validate_dir(dir_path)
     successes, fails = dict(), dict()
-    for file in dir_path.glob('*'):
+    for file in dir_path.iterdir():
         try:
             successes[file] = func(file)
         except Exception as e:
@@ -17,8 +17,8 @@ def validate_dir(dir_path):
     if not (isinstance(dir_path, str) or isinstance(dir_path, Path)):
         raise ValueError('Directory path must be string or Path object')
 
-    directory = Path(dir_path)
-    if not directory.is_dir():
+    dir_path = Path(dir_path)
+    if not dir_path.is_dir():
         raise ValueError(f'{dir_path} is not a directory')
 
-    return directory
+    return dir_path
