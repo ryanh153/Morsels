2dc92240ad9a8ba06fe5d87bb1710ae81f927772
Ryan Horton <rhorton@scitec.com>
2021-05-11 08:51:30 -0600

    Added dirname as property which I like better

--- B1_course/week11/solution.py
+++ B1_course/week11/solution.py
@@ -5,10 +5,13 @@ from pathlib import Path
 class DirFileHash:
 
     def __init__(self, dir_path):
-        self.dirname = dir_path
-        self.dir_path = Path(dir_path)
+        self.dir = Path(dir_path)
 
     def __getitem__(self, item):
-        if (file := self.dir_path.joinpath(item)).exists():
+        if (file := self.dir.joinpath(item)).exists():
             return md5(open(file, 'rb').read()).hexdigest()
         return None
+
+    @property
+    def dirname(self):
+        return str(self.dir)
