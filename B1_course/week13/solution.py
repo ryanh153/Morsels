19bc1b44c7b68ce96d3a538ca3ada387f42d8cb7
Ryan Horton <rhorton@scitec.com>
2021-05-24 11:45:09 -0600

    Fix mypy errors. Fun playing with that and full annotation

--- B1_course/week13/solution.py
+++ B1_course/week13/solution.py
@@ -1,51 +1,42 @@
-from __future__ import annotations
+from functools import total_ordering
+import operator
+from numbers import Real
+from typing import Union, Any, Callable
 
-from typing import Union
-
-numeric = Union[int, float]
+# Note: NotImplementedType is not in python 3.9 (to be reintroduced in python 3.10). Using Any as is standard practice
 
 
+@total_ordering
 class ThresholdEqual:
-    """Stores a value and a threshold (default zero) for comparing equality"""
+    """Stores a numeric value and a threshold (default 2) for comparing equality"""
 
-    def __init__(self, value: numeric, threshold: numeric = 2) -> None:
-        """Takes value (numeric) and an optional threshold to be used in comparisons"""
+    def __init__(self, value: Real, threshold: float = 2.0) -> None:
+        """Takes value (numeric) and an optional threshold to be used in comparisons
+        Sets up all mathematical operations"""
         self.value = value
         self.threshold = threshold
 
-    def __eq__(self, other: object) -> bool:
-        """If other is numeric and the difference between it and our value is within the threshold return True"""
+    def __eq__(self, other: object) -> Union[bool, Any]:
+        """Define equality based on value for total ordering"""
         if isinstance(other, ThresholdEqual):
             return abs(self.value - other.value) <= self.threshold
         return NotImplemented
 
-    # Implement all relevant mathematics operations
-    def __add__(self, other: ThresholdEqual) -> numeric:
+    def __lt__(self, other: object) -> Union[bool, Any]:
+        """Define less than based on value for total ordering"""
         if isinstance(other, ThresholdEqual):
-            return self.value + other.value
+            return abs(self.value - other.value) <= self.threshold
         return NotImplemented
 
-    def __sub__(self, other: ThresholdEqual) -> numeric:
-        if isinstance(other, ThresholdEqual):
-            return self.value - other.value
-        return NotImplemented
 
-    def __mul__(self, other: ThresholdEqual) -> numeric:
-        if isinstance(other, ThresholdEqual):
-            return self.value * other.value
-        return NotImplemented
+def apply_func(func: Callable[[Real, Real], Real]) -> Callable[[ThresholdEqual, ThresholdEqual], Union[bool, Any]]:
+    """Take a function that takes the value attribute two ThresholdEqual objects.
+    It then returns a callable that can be passed just the ThresholdEqual objects themselves
+    Used for adding methods to the ThresholdEqual class easily"""
+    def inner(self: ThresholdEqual, other: ThresholdEqual) -> Union[bool, Any]:
+        return func(self.value, other.value)
+    return inner
 
-    def __truediv__(self, other: ThresholdEqual) -> numeric:
-        if isinstance(other, ThresholdEqual):
-            return self.value / other.value
-        return NotImplemented
 
-    def __mod__(self, other: ThresholdEqual) -> numeric:
-        if isinstance(other, ThresholdEqual):
-            return self.value % other.value
-        return NotImplemented
-
-    def __pow__(self, other: ThresholdEqual) -> numeric:
-        if isinstance(other, ThresholdEqual):
-            return self.value ** other.value
-        return NotImplemented
+for function in ['__add__', '__sub__', '__mul__', '__truediv__', '__mod__', '__pow__']:
+    setattr(ThresholdEqual, function, apply_func(getattr(operator, function)))
