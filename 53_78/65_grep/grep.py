3108a70f02b754e20779b27e48f92e6f30f9523a
Ryan Horton <rhorton@scitec.com>
2021-06-09 08:40:03 -0600

    Tests didn't work if inside __name__ == '__main__'. Solid problem though

--- 53_78/65_grep/grep.py
+++ 53_78/65_grep/grep.py
@@ -15,8 +15,8 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def search_file(re_str: str, file: Path, ignore_case: bool, add_line_number: bool, invert_matching: bool,
-                initial_tab: bool) -> None:
+def search_file(re_str: str, file: Path, ignore_case: bool, add_line_number: bool, show_filename: bool,
+                invert_matching: bool, initial_tab: bool) -> None:
     if not file.exists():
         raise ValueError(f'grep passed file that does not exist.\nPassed {file}')
     if ignore_case:
@@ -25,7 +25,7 @@ def search_file(re_str: str, file: Path, ignore_case: bool, add_line_number: boo
     file_size = file.stat().st_size
     for line_number, (original, altered) in enumerate(iter_lines(file, ignore_case)):
         if should_display(altered, re_str, invert_matching):
-            print_result(original, file, add_line_number, line_number, initial_tab, file_size)
+            print_result(original, file, add_line_number, line_number, show_filename, initial_tab, file_size)
 
 
 def should_display(string: str, re_str: str, invert_matching: bool) -> bool:
@@ -40,12 +40,17 @@ def iter_lines(file: Path, ignore_case: bool) -> Generator[Tuple[str, str], None
             yield (line, line.lower()) if ignore_case else (line, line)
 
 
-def print_result(string: str, file: Path, add_line_number: bool, line_number: int, initial_tab: bool, file_size: int)\
-        -> None:
-    file_expression = file if add_line_number else ''
-    line_expression = line_number if add_line_number else ''
-    spacer = get_spacer(initial_tab, add_line_number, file_size)
-    print(f'{file_expression}{spacer}{line_expression}{spacer}{string}', end='')
+def print_result(string: str, file: Path, add_line_number: bool, line_number: int, show_filename: bool,
+                 initial_tab: bool, file_size: int) -> None:
+    file_expression = f'{file}:' if show_filename else ''
+    file_spacer = '\t' if initial_tab and show_filename else ''
+
+    # Converting to string and get length -> gives number of digits in number
+    line_number_width = len(str(file_size))+1 if initial_tab else 0
+    line_expression = f'{line_number+1}:'.rjust(line_number_width, ' ') if add_line_number else ''
+    line_spacer = '\t' if initial_tab else ''
+
+    print(f'{file_expression}{file_spacer}{line_expression}{line_spacer}{string}', end='')
 
 
 def get_spacer(initial_tab: bool, add_line_number: bool, file_size: int) -> str:
@@ -54,8 +59,8 @@ def get_spacer(initial_tab: bool, add_line_number: bool, file_size: int) -> str:
     return ''.join([' ' for _ in range(len(str(file_size))-1)])
 
 
-if __name__ == '__main__':
-    args = parse_args()
-    for file_to_parse in args.files:
-        search_file(args.re_str, file_to_parse, args.ignore_case, args.line_number, args.invert_match, args.initial_tab)
-        print('', flush=True)
+args = parse_args()
+display_filename = len(args.files) > 1
+for file_to_parse in args.files:
+    search_file(args.re_str, file_to_parse, args.ignore_case, args.line_number, display_filename, args.invert_match,
+                args.initial_tab)
