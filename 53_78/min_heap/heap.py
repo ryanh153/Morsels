2b390450b967a861151fe57ba76325cf0415bd69
Ryan Horton <rhorton@scitec.com>
2021-10-07 09:55:04 -0600

    More blocked out class structure for third bonus

--- 53_78/min_heap/heap.py
+++ 53_78/min_heap/heap.py
@@ -1,79 +1,44 @@
-import heapq
+from heapq import heapify, heappop, heappush
+
+
+class Comparator:
+
+    def __init__(self, value, key=None):
+        self.key = key
+        self.data = (value if key is None else key(value), value)
+        self.item = value
+
+    def __lt__(self, other):
+        return self.data < other.data
+
+
+class InverseComparator(Comparator):
+
+    def __lt__(self, other):
+        return other.data < self.data
 
 
 class MinHeap:
 
-    def __init__(self, iterable, key=lambda x: x):
-        self.key = key
-        self.heap = [val for val in iterable]
-        self._sort_heap()
+    COMPARATOR = Comparator
 
-    def _sort_heap(self):
-        self.heap = sorted(self.heap, key=self.key, reverse=True)
+    def __init__(self, iterable, key=None):
+        self.key = key
+        self.heap = [self.COMPARATOR(val, key) for val in iterable]
+        heapify(self.heap)
 
     def peek(self):
-        return self.heap[-1]
+        return self.heap[0].item
 
     def pop(self):
-        return self.heap.pop()
+        return heappop(self.heap).item
 
     def push(self, item):
-        self.heap.append(item)
-        self._sort_heap()
+        heappush(self.heap, self.COMPARATOR(item, self.key))
 
     def __len__(self):
         return len(self.heap)
 
 
 class MaxHeap(MinHeap):
-
-    def _sort_heap(self):
-        self.heap = sorted(self.heap, key=self.key, reverse=False)
-
-
-## Real
-# import heapq
-# import heapq_max
-#
-#
-# class MinHeap:
-#
-#     def __init__(self, iterable, key=lambda x: x):
-#         # Copy data and store comparable value and original value as the first and second items in a tuple
-#         self.heap = [(key(val), val) for val in iterable]
-#         heapq.heapify(self.heap)
-#         self.key = key
-#
-#     def peek(self):
-#         return heapq.nsmallest(1, self.heap)[0][1]
-#
-#     def pop(self):
-#         return heapq.heappop(self.heap)[1]
-#
-#     def push(self, item):
-#         heapq.heappush(self.heap, (self.key(item), item))
-#
-#     def __len__(self):
-#         return len(self.heap)
-#
-#
-# class MaxHeap:
-#
-#     def __init__(self, iterable, key=lambda x: x):
-#         # Copy data and store comparable value and original value as the first and second items in a tuple
-#         self.heap = [(key(val), val) for val in iterable]
-#         heapq_max.heapify_max(self.heap)
-#         self.key = key
-#
-#     def peek(self):
-#         return heapq.nlargest(1, self.heap)[0][1]
-#
-#     def pop(self):
-#         return heapq_max.heappop_max(self.heap)[1]
-#
-#     def push(self, item):
-#         heapq_max.heappush_max(self.heap, (self.key(item), item))
-#
-#     def __len__(self):
-#         return len(self.heap)
-
+    COMPARATOR = InverseComparator
