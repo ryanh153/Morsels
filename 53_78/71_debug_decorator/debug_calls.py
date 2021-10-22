e1d79384c45c5789c188304627fb1a7ce4db5891
Ryan Horton <rhorton@scitec.com>
2021-10-21 18:08:43 -0600

    Update debug calls with better signature use and setting it up like a func that returns a decorator

--- 53_78/71_debug_decorator/debug_calls.py
+++ 53_78/71_debug_decorator/debug_calls.py
@@ -3,54 +3,46 @@ import functools
 import pdb
 
 
-def debug_calls(func=None, *, set_break=None):
-    # If we're called with an argument we don't get the function (it's not passed)
-    # So return a version of ourselves with set break set accordingly
-    # That will be called and we'll be good
-    if func is None:
-        return functools.partial(debug_calls, set_break=set_break)
+def make_call_string(func, *args, **kwargs):
+    frame_info = inspect.getouterframes(inspect.currentframe(), 2)[2]
+    return f'{func.__name__}('\
+           f'{", ".join([repr(arg) for arg in args])}'\
+           f'{", " if len(args) and len(kwargs) else ""}'\
+           f'{", ".join(f"{str(key)}={repr(val)}" for key, val in kwargs.items())}'\
+           f') called by '\
+           f'{frame_info.function} '\
+           f'in file {frame_info.filename} '\
+           f'on line {frame_info.lineno}'
 
-    # Set non-locals
-    first = True
-    condition = ''
 
-    @functools.wraps(func)
-    def inner(*args, **kwargs):
-        nonlocal first, condition, set_break
-        break_set = False
-        frame_info = inspect.getouterframes(inspect.currentframe(), 2)[1]
-        # Put our arguments into the local namespace
-        call_dict = {name: value for name, value in
-                     zip(inspect.getfullargspec(func).args, args)}
-        call_dict.update(kwargs)
-        locals().update(call_dict)
+def debug_calls(func=None, *, set_break=False):
 
-        # Set condition if doing so
-        if first and set_break:
-            condition = input(f'Debug {func.__name__} when?: ')
-            first = False
+    def decorator(function):
 
-        # Check for breaks
-        if set_break and eval(condition):
-            print(f'Condition met: {condition}')
-            break_set = True
+        @functools.wraps(function)
+        def inner(*args, **kwargs):
+            print(make_call_string(function, *args, **kwargs))
 
-        elif set_break and not eval(condition):
-            print(f'Condition not met: {condition}')
+            if set_break:
+                if not inner.break_condition:
+                    inner.break_condition = input(f'Debug {function.__name__} when?: ')
 
-        first = False
-        print(f'{func.__name__}('
-              f'{", ".join([repr(arg) for arg in args])}'
-              f'{", " if len(args) and len(kwargs) else ""}'
-              f'{", ".join(f"{str(key)}={repr(val)}" for key, val in kwargs.items())}'
-              f') called by '
-              f'{frame_info.function} '
-              f'in file {frame_info.filename} '
-              f'on line {frame_info.lineno}')
+                # Get bound function namespace for eval
+                signature = inspect.signature(function).bind(*args, **kwargs)
+                signature.apply_defaults()
 
-        if break_set:
-            return pdb.runcall(func, *args, **kwargs)
-        else:
-            return func(*args, **kwargs)
+                if eval(inner.break_condition, signature.arguments):
+                    print(f'Condition met: {inner.break_condition}')
+                    return pdb.runcall(function, *args, **kwargs)
+                else:
+                    print(f'Condition not met: {inner.break_condition}')
 
-    return inner
+            return function(*args, **kwargs)
+
+        inner.break_condition = None
+        return inner
+
+    if func is None:  # Called w/ arguments. Return decorator to be called
+        return decorator
+    else:  # Called as decorator with no arguments
+        return decorator(func)
