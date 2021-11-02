fa323ed41ea11291c26353a785319bfef318466c
Ryan Horton <rhorton@scitec.com>
2021-11-02 10:23:55 -0600

    Got it! Damn that was crazy

--- 53_78/73_warningutils/warningutils.py
+++ 53_78/73_warningutils/warningutils.py
@@ -1,4 +1,3 @@
-import contextlib
 import warnings
 from functools import wraps
 import re
@@ -27,20 +26,24 @@ class error_on_warnings:
 
 class NiceWarnings:
 
-    def __init__(self, warning_list, message, category):
-        self.warning_list = warning_list
+    def __init__(self, message, category):
+        self.warning_list = list()
         self.message = message
         self.category = category
 
-    @property
-    def unmatched_results(self):
-        return [NiceWarning(warning) for warning in self.warning_list
-                if not self.match(warning)]
+    def add_warning(self, warning):
+        self.warning_list.append(warning)
 
     @property
     def results(self):
-        return [NiceWarning(warning) for warning in self.warning_list
-                if self.match(warning)]
+        caught_warnings = set()
+        results = list()
+        for warning in self.warning_list:
+            if self.match(warning) and warning.category not in caught_warnings:
+                results.append(NiceWarning(warning))
+                caught_warnings.add(warning.category)
+
+        return results
 
     def match(self, warning):
         return issubclass(warning.category, self.category) and bool(re.match(self.message, str(warning.message)))
@@ -58,7 +61,6 @@ class NiceWarnings:
 class NiceWarning:
 
     def __init__(self, warning):
-        self.warning = warning
         for name in dir(warning):
             if not name.startswith('_'):
                 setattr(self, name, getattr(warning, name))
@@ -67,11 +69,29 @@ class NiceWarning:
         return f'WarningMessage(message={self.message}, category={self.category}, ' \
                f'filename={self.filename}, lineno={self.lineno}'
 
+class capture_warnings:
 
-@contextlib.contextmanager
-def capture_warnings(message='', category=Warning):
-    with warnings.catch_warnings(record=True) as warnings_list:
-        nice_warnings = NiceWarnings(warnings_list, message, category)
-        yield nice_warnings
-    for warning in nice_warnings.unmatched_results:
-        warnings.warn(warning.warning)
+    contexts = list()
+
+    def __init__(self, message='', category=Warning):
+        self.message = message
+        self.category = Warning
+        self.original_msg_func = warnings._showwarnmsg_impl
+        self.caught_warnings = NiceWarnings(message=message, category=category)
+
+    def __enter__(self):
+        warnings._showwarnmsg_impl = self.handle_warning
+        self.contexts.append(self.caught_warnings)
+        return self.caught_warnings
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        warnings._showwarnmsg_impl = self.original_msg_func
+        self.contexts.pop(-1)
+
+    def handle_warning(self, warning):
+        for context in self.contexts[::-1]:  # Search through contexts backwards through scopes
+            if context.match(warning):  # Put in the deepest context it matches
+                context.add_warning(warning)
+                break
+        else:  # If it doesn't match any context -> warn
+            self.original_msg_func(warning)
