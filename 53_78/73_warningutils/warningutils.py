a084c5bfb65c221f0c68e4b63a49b797c8351f29
Ryan Horton <rhorton@scitec.com>
2021-11-02 10:29:16 -0600

    Clean up warningutils

--- 53_78/73_warningutils/warningutils.py
+++ 53_78/73_warningutils/warningutils.py
@@ -4,6 +4,8 @@ import re
 
 
 class error_on_warnings:
+    """Context manager/decorator for raising errors on specific warnings"""
+
     def __init__(self, message='', category=Warning):
         self.message = message
         self.category = category
@@ -25,6 +27,7 @@ class error_on_warnings:
 
 
 class NiceWarnings:
+    """Collect a list of NiceWarning instances that pass certain criteria based on the message and the type"""
 
     def __init__(self, message, category):
         self.warning_list = list()
@@ -32,21 +35,17 @@ class NiceWarnings:
         self.category = category
 
     def add_warning(self, warning):
-        self.warning_list.append(warning)
-
-    @property
-    def results(self):
-        caught_warnings = set()
-        results = list()
-        for warning in self.warning_list:
-            if self.match(warning) and warning.category not in caught_warnings:
-                results.append(NiceWarning(warning))
-                caught_warnings.add(warning.category)
-
-        return results
+        if self.match(warning):
+            self.warning_list.append(warning)
+            return True
+        return False
 
     def match(self, warning):
         return issubclass(warning.category, self.category) and bool(re.match(self.message, str(warning.message)))
+
+    @property
+    def results(self):
+        return [NiceWarning(warning) for warning in self.warning_list]
 
     def __getitem__(self, index):
         return self.results[index]
@@ -59,7 +58,7 @@ class NiceWarnings:
 
 
 class NiceWarning:
-
+    """Mirrors attributes in a warning instance but provides a nice string representation"""
     def __init__(self, warning):
         for name in dir(warning):
             if not name.startswith('_'):
@@ -69,7 +68,9 @@ class NiceWarning:
         return f'WarningMessage(message={self.message}, category={self.category}, ' \
                f'filename={self.filename}, lineno={self.lineno}'
 
+
 class capture_warnings:
+    """Context manager that stores certain warnings. Warnings that do not match are still raised as warnings"""
 
     contexts = list()
 
@@ -90,8 +91,7 @@ class capture_warnings:
 
     def handle_warning(self, warning):
         for context in self.contexts[::-1]:  # Search through contexts backwards through scopes
-            if context.match(warning):  # Put in the deepest context it matches
-                context.add_warning(warning)
+            if context.add_warning(warning):  # Put in the deepest context it matches
                 break
         else:  # If it doesn't match any context -> warn
             self.original_msg_func(warning)
