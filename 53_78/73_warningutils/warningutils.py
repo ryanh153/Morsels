011ce896d094d1c80929fa103341671be7ae5665
Ryan Horton <rhorton@scitec.com>
2021-11-11 17:38:08 -0700

    Update with final solution

--- 53_78/73_warningutils/warningutils.py
+++ 53_78/73_warningutils/warningutils.py
@@ -1,97 +1,39 @@
-import warnings
-from functools import wraps
+from contextlib import contextmanager
+from dataclasses import dataclass
 import re
+import warnings
 
 
-class error_on_warnings:
-    """Context manager/decorator for raising errors on specific warnings"""
-
-    def __init__(self, message='', category=Warning):
-        self.message = message
-        self.category = category
-
-    def __enter__(self):
-        warnings.filterwarnings('error', message=self.message, category=self.category)
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        warnings.filters.pop(0)
-
-    def __call__(self, func):
-        @wraps(func)
-        def decorated(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
-
-        return decorated
+@contextmanager
+def error_on_warnings(message='', category=Warning):
+    with warnings.catch_warnings():
+        warnings.filterwarnings('error', message, category)
+        yield
 
 
-class NiceWarnings:
-    """Collect a list of NiceWarning instances that pass certain criteria based on the message and the type"""
-
-    def __init__(self, message, category):
-        self.warning_list = list()
-        self.message = message
-        self.category = category
-
-    def add_warning(self, warning):
-        if self.match(warning):
-            self.warning_list.append(warning)
-            return True
-        return False
-
-    def match(self, warning):
-        return issubclass(warning.category, self.category) and bool(re.match(self.message, str(warning.message)))
-
-    @property
-    def results(self):
-        return [NiceWarning(warning) for warning in self.warning_list]
-
-    def __getitem__(self, index):
-        return self.results[index]
-
-    def __len__(self):
-        return len(self.results)
-
-    def __repr__(self):
-        return f'[{", ".join([repr(warning) for warning in self.results])}]'
-
-
+@dataclass
 class NiceWarning:
-    """Mirrors attributes in a warning instance but provides a nice string representation"""
-    def __init__(self, warning):
-        for name in dir(warning):
-            if not name.startswith('_'):
-                setattr(self, name, getattr(warning, name))
-
-    def __repr__(self):
-        return f'WarningMessage(message={self.message}, category={self.category}, ' \
-               f'filename={self.filename}, lineno={self.lineno}'
+    message: Warning
+    category: type
+    filename: str
+    lineno: int
 
 
-class capture_warnings:
-    """Context manager that stores certain warnings. Warnings that do not match are still raised as warnings"""
+@contextmanager
+def capture_warnings(message='', category=Warning):
+    regex = re.compile(message)
+    target_category = category
 
-    contexts = list()
+    def log_warnings(message_to_log, category_to_log, filename, lineno, *args, **kwargs):
+        if regex.match(str(message_to_log)) and issubclass(category_to_log, target_category):
+            logs.append(NiceWarning(message_to_log, category_to_log, filename, lineno))
+        else:
+            showwarning(message_to_log, category_to_log, filename, lineno, *args, **kwargs)
 
-    def __init__(self, message='', category=Warning):
-        self.message = message
-        self.category = Warning
-        self.original_msg_func = warnings._showwarnmsg_impl
-        self.caught_warnings = NiceWarnings(message=message, category=category)
-
-    def __enter__(self):
-        warnings._showwarnmsg_impl = self.handle_warning
-        self.contexts.append(self.caught_warnings)
-        return self.caught_warnings
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        warnings._showwarnmsg_impl = self.original_msg_func
-        self.contexts.pop(-1)
-
-    def handle_warning(self, warning):
-        for context in self.contexts[::-1]:  # Search through contexts backwards through scopes
-            if context.add_warning(warning):  # Put in the deepest context it matches
-                break
-        else:  # If it doesn't match any context -> warn
-            self.original_msg_func(warning)
+    logs = []
+    showwarning = warnings.showwarning
+    warnings.showwarning = log_warnings
+    try:
+        yield logs
+    finally:
+        warnings.showwarning = showwarning
