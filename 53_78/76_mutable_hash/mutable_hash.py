ecf028ff2d3fde20a117258627ca27f376dc3641
ryan <ryan.h153@gmail.com>
2021-12-08 18:36:30 -0700

    Ok. Now with single dispatch

--- 53_78/76_mutable_hash/mutable_hash.py
+++ 53_78/76_mutable_hash/mutable_hash.py
@@ -1,59 +1,36 @@
 from collections import UserDict
+from functools import singledispatch
 
 
 def hashable(value):
     return getattr(value, '__hash__') is not None
 
 
-class mutable_hash:
+@singledispatch
+def mutable_hash(data):
     """
-    Gross way to make it so we can register types and functions for our mutable hash function
-    """
-
-    registered_types = dict()
-
-    def __new__(cls, data):
-        return cls._mutable_hash(data)
-
-    @classmethod
-    def register(cls, data_type):
-        def decorator(func):
-            cls.registered_types[data_type] = func
-
-            def inner(*args, **kwargs):
-                return func(*args, **kwargs)
-            return inner
-
-        return decorator
-
-    @classmethod
-    def _mutable_hash(cls, data):
+        Hash an object (mutable or not)
+        :param data:
+            if data is
+            - a registered type : Return that types corresponding has function
+            - hashable : return the objects hash
+            - list : Hash where order and value matters
+            - set : Hash where only values matters
+            - dict : Hash where we care about which key, value pairs are in the dictionary but not their order
+        :return:
+            Hash of the object as defined above.
         """
-            Hash an object (mutable or not)
-            :param data:
-                if data is
-                - a registered type : Return that types corresponding has function
-                - hashable : return the objects hash
-                - list : Hash where order and value matters
-                - set : Hash where only values matters
-                - dict : Hash where we care about which key, value pairs are in the dictionary but not their order
-            :return:
-                Hash of the object as defined above.
-            """
-        for registered_type, registered_function in cls.registered_types.items():
-            if isinstance(data, registered_type):
-                return registered_function(data)
-        if hashable(data):
-            if hasattr(data, '__getitem__') and any([not hashable(value) for value in data]):
-                return hash(tuple([value if hashable(value) else mutable_hash(value) for value in data]))
-            return hash(data)
-        if isinstance(data, list):
-            return hash(tuple(data))
-        if isinstance(data, set):
-            return sum([hash(val) for val in data])
-        if isinstance(data, dict):
-            return sum([mutable_hash(item) for item in data.items()])
-        raise TypeError(f'Data type {type(data)} is not implemented yet')
+    if hashable(data):
+        if hasattr(data, '__getitem__') and any([not hashable(value) for value in data]):
+            return hash(tuple([value if hashable(value) else mutable_hash(value) for value in data]))
+        return hash(data)
+    if isinstance(data, list):
+        return hash(tuple(data))
+    if isinstance(data, set):
+        return sum([hash(val) for val in data])
+    if isinstance(data, dict):
+        return sum([mutable_hash(item) for item in data.items()])
+    raise TypeError(f'Data type {type(data)} is not implemented yet')
 
 
 class HashWrapper:
